bc44b099700ea5ac26292efcff4c5644bf382d40
Manikanta CH <manikanta.23cs064@sode-edu.in>
2026-09-24 22:15:41 +0530

    Prepare server for cloud deployment

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const { tunnel: cloudflaredTunnel } = require("cloudflared")
 const cookieParser = require("cookie-parser")
 const socketIO = require("socket.io")
 const config = require("./config")
 const express = require("express")
 const tarkine = require("tarkine")
-const http = require('http')
+const http = require("http")
 
 const app = express()
 const server = http.createServer(app)
 const io = new socketIO.Server(server)
+
 const PORT = process.env.PORT || config.port
-global.remoteURL
 
 global.IO = io
 
 app.set("view engine", "html")
 app.engine("html", tarkine.renderFile)
+
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(__dirname + "/public"))
@@ -23,12 +23,6 @@ app.use(express.json())
 
 app.use("/", require("./router"))
 
-server.listen(PORT, async () => {
-    const localURL = `http://localhost:${PORT}`
-    remoteURL = await cloudflaredTunnel({
-        "--url": localURL
-    }).url
-
-    console.log(`LOCAL  : ${localURL}`)
-    console.log(`REMOTE : ${remoteURL}`)
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`)
 })
